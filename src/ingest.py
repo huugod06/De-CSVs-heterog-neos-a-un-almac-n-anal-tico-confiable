e96862e05248c1a6d0f1a936125c24dcc6bdddc9
huugod06 <hugodediosbasanta@gmail.com>
2025-09-24 14:49:04 +0200

    Create ingest.py

---
+++ src/ingest.py
@@ -0,0 +1,8 @@
+# Script de ingesta
+# Responsabilidad: cargar CSVs heterogéneos a la capa Bronze.
+
+def ingest_csv(input_path: str, output_path: str):
+    """
+    Ingesta un archivo CSV desde 'input_path' y lo guarda en 'output_path/bronze'.
+    """
+    pass
