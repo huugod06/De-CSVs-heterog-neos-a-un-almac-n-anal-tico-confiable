e864534b0e73e12f52bbb0fd3a824d497a9dba64
huugod06 <hugodediosbasanta@gmail.com>
2025-09-24 14:48:00 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+# Punto de entrada para la app de Streamlit
+# Aquí se leerán los datos desde la capa "silver"
+# y se mostrarán KPIs y visualizaciones interactivas.
+
+import streamlit as st
+
+st.title("Big Data Storage Lab - <dedios>")
+st.write("🚀 Dashboard de KPIs sobre datos procesados (Silver Layer).")
