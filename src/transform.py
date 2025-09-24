14117e873627fc14b9597493bd67a2049f6f3ebb
huugod06 <hugodediosbasanta@gmail.com>
2025-09-24 14:52:41 +0200

    Create transform.py

---
+++ src/transform.py
@@ -0,0 +1,8 @@
+# Script de transformación
+# Responsabilidad: normalizar y limpiar datos de Bronze → Silver.
+
+def transform_data(df):
+    """
+    Transforma datos de la capa Bronze para generar la capa Silver.
+    """
+    pass
